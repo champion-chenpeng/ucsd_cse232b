fc420bdbec0ad6051fb8a7fc1a7914d06c31cb02
Chen Peng <65433575+champion-chenpeng@users.noreply.github.com>
2024-02-05 22:39:25 -0800

    Add all process test and result content compare test.

--- src/test/java/com/cse232b/AppTest.java
+++ src/test/java/com/cse232b/AppTest.java
@@ -4,17 +4,43 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import org.w3c.dom.Node;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+	 * Test whether the pipeline runs well
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	public void SingleQueryTest() {
+		try (
+			InputStream testXPathInStream = AppTest.class.getClassLoader().getResourceAsStream("XPath0.txt");
+			OutputStream testXPathOutStream = new FileOutputStream("XPath0_result.xml");
+		) {
+			List<Node> rawResult = XPathEvaluator.evaluateXPath(testXPathInStream);
+			XMLProcessor.generateResultXMLThenOutput(rawResult, testXPathOutStream, true);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void ContentCompareTest() {
+		try (
+			InputStream ResultInStream = new FileInputStream("XPath0_result.xml");
+			InputStream RefInStream = new FileInputStream("XPath0_result_standard.xml");
+		) {
+			assert(IOUtils.contentEquals(RefInStream, ResultInStream));	
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
