86d6885782d4dc7a44f4d2351e40d77a048ab1dc
Chen Peng <65433575+champion-chenpeng@users.noreply.github.com>
2024-03-14 00:06:11 -0700

    Add all 10 xpath and xquery test.

--- src/test/java/com/cse232b/AppTest.java
+++ src/test/java/com/cse232b/AppTest.java
@@ -16,58 +16,50 @@ import java.util.List;
  */
 public class AppTest 
 {
-	private int NXPathCase = 10, NXQueryCase = 2;
+	private int NXPathCase = 10, NXQueryCase = 10;
     /**
 	 * Test whether the pipeline runs well
      */
-    @Test
-	public void XPathQueryTest() {
+	private void query(String type) {
 		try {
-			for (int i = 0; i < NXPathCase; i++) {
-				System.out.println("Query xpath file %d".formatted(i));
-				Main.main(new String[] { "src/test/resources/xquery/xpath/XPath%d.txt".formatted(i), "target/test-classes/XPath%d_result.xml".formatted(i) });
+			for (int i = 1; i <= NXPathCase; i++) {
+				System.out.println("Query " + type + " file %d".formatted(i));
+				Main.main(new String[] { "src/test/resources/" + type + "/query/query%d.txt".formatted(i), "target/test-classes/" + type + "%d.xml".formatted(i) });
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private void contentCompare(String type) {
+		try {
+			for (int i = 1; i <= NXPathCase; i++) {
+				System.out.println("Comparing " + type + " File %d".formatted(i));
+				InputStream ResultInStream = new FileInputStream("target/test-classes/" + type + "%d.xml".formatted(i));
+				InputStream RefInStream = new FileInputStream("src/test/resources/" + type + "/result/query%d.xml".formatted(i));
+				assert(IOUtils.contentEquals(RefInStream, ResultInStream));	
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+		
+    @Test
+	public void XPathQueryTest() {
+		query("xpath");
 	}
 
 	@Test
 	public void XPathContentCompareTest() {
-		try {
-			for (int i = 0; i < NXPathCase; i++) {
-				System.out.println("Compare xpath File %d".formatted(i));
-				InputStream ResultInStream = new FileInputStream("target/test-classes/XPath%d_result.xml".formatted(i));
-				InputStream RefInStream = new FileInputStream("src/test/resources/xpath/XPath%d_result_standard.xml".formatted(i));
-				assert(IOUtils.contentEquals(RefInStream, ResultInStream));	
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		contentCompare("xpath");
 	}
     @Test
 	public void XQueryTest() {
-		try {
-			for (int i = 0; i < NXQueryCase; i++) {
-				System.out.println("Query xquery file %d".formatted(i));
-				Main.main(new String[] { "src/test/resources/xquery/XQuery%d.txt".formatted(i), "target/test-classes/XQuery%d_result.xml".formatted(i) });
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		query("xquery");
 	}
 
 	@Test
 	public void XQueryContentCompareTest() {
-		try {
-			for (int i = 0; i < NXQueryCase; i++) {
-				System.out.println("Compare File %d".formatted(i));
-				InputStream ResultInStream = new FileInputStream("target/test-classes/XQuery%d_result.xml".formatted(i));
-				InputStream RefInStream = new FileInputStream("src/test/resources/xquery/XQuery%d_result_standard.xml".formatted(i));
-				assert(IOUtils.contentEquals(RefInStream, ResultInStream));	
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		contentCompare("xquery");
 	}
 }
